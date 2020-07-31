71a72f8376af04c6d26d77d74a2872e273ad262b
rorymclaurin <68992648+rorymclaurin@users.noreply.github.com>
2020-07-31 17:16:26 +0100

    Update control.m
    Added input solar_intensity so that day-night cycle can be tracked - in line with suggested code in new documentation

--- control.m
+++ control.m
@@ -1,36 +1,8 @@
-function [heat] = control(temperatures,time)
+function [heat] = control(temperatures,time,solar_intensity)
 %Models thermal control systems in the spacecraft. 
 %Use the time value to build scheduled changes, such as turning on heaters
-%or heat pipes
 
 heat = zeros(size(temperatures,1),1);
-
-
-%spot heaters (night)
-
-% if mod(time,672*3600)>(1-eclipse_fraction)*672*3600
-    
-    % heat(12,1) = heat(12,1) + 10; %10 watts heating to WEB
-                 
-    % heat(13,1) = heat(13,1) + 5; %5 watts heating to Antenna
-
-    
-% end  
-
-%spot heaters (temperature dependent)
-
-%WEB
-
-% if temperatures(12,1)<293
-    
-    % heat(12,1) = heat(12,1)+10; %10 watts heating
-    
-% end
-
-
-            
-
-
-        
+      
 end
 
