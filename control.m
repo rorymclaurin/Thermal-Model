af3dfd4a76643c2589f7d7c81b5e7735335df358
rorymclaurin <68992648+rorymclaurin@users.noreply.github.com>
2020-07-31 11:17:13 +0100

    Update control.m
    Removing control_track in line with request - redundant variable used during development

--- control.m
+++ control.m
@@ -1,28 +1,19 @@
-function [heat,control_track] = control(temperatures,time,control_track)
+function [heat,control_track] = control(temperatures,time)
 %Models thermal control systems in the spacecraft. 
 %Use the time value to build scheduled changes, such as turning on heaters
 %or heat pipes
 
 heat = zeros(size(temperatures,1),1);
 
-control_track = 0*control_track;
-
-control_track(1,1) = time;
-
-%add power consumption?
 
 %spot heaters (night)
 
 % if mod(time,672*3600)>(1-eclipse_fraction)*672*3600
     
-    % heat(12,1) = heat(12,1) + 10;%10 watts heating to WEB
-    
-    % control_track(2,1) = 10;
-            
-            
+    % heat(12,1) = heat(12,1) + 10; %10 watts heating to WEB
+                 
     % heat(13,1) = heat(13,1) + 5; %5 watts heating to Antenna
-    
-    % control_track(3,1) = 5;
+
     
 % end  
 
@@ -34,17 +25,9 @@ control_track(1,1) = time;
     
     % heat(12,1) = heat(12,1)+10; %10 watts heating
     
-    % control_track(4,1) = 10;
-    
 % end
 
 
-
-
-
-%sum tracker
-
-control_track(size(control_track,1),1) = sum(control_track(2:size(control_track,1)-1),1);
             
 
 
