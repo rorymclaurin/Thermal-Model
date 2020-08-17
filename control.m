0272c8dd68144bb2199f332a319daa9dcd8f52b2
rorymclaurin <68992648+rorymclaurin@users.noreply.github.com>
2020-08-17 10:25:55 +0100

    Update control.m
    Updated comment in line with new night-time definition

--- control.m
+++ control.m
@@ -1,6 +1,6 @@
 function [heat] = control(temperatures,time,solar_intensity)
 %Models thermal control systems in the spacecraft. 
-%Use the time value to build scheduled changes, such as turning on heaters
+%Use the solar_intensity value to build scheduled changes, such as turning on heaters
 
 heat = zeros(size(temperatures,1),1);
       
